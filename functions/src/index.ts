ac6eb1097f8248817f0f06008bb145f1d1c53d70
Enrique Oriol <enrique.oriol@dynatrace.com>
2019-10-11 17:56:52 +0200

    step 4: update server to be executed as firebase function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,5 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const universal = functions.https.onRequest((request, response) => {
+  require(`${process.cwd()}/dist/server`).app(request, response);
+});
--- server.ts
+++ server.ts
@@ -18,16 +18,16 @@
 import 'zone.js/dist/zone-node';
 
 import * as express from 'express';
-import {join} from 'path';
+import { join } from 'path';
 
 // Express server
-const app = express();
+export const app = express();
 
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap } = require('./dist/server/main');
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
